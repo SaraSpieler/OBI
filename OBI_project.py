6bcad04712a62e7c0bab4af2e040396d07eb32a5
Lucien-Piat <134285376+Lucien-Piat@users.noreply.github.com>
2023-11-15 12:26:43 +0100

    Update OBI_project.py

--- OBI_project.py
+++ OBI_project.py
@@ -20,10 +20,52 @@ def read_fasta(filename):
     dico[id]=content
     return dico
 
-seq = read_fasta('test_prep.fa')
-print(seq)
-seq = read_fasta('test_prot.fa')
-print(seq)
+seq_pep = read_fasta('test_pep.fasta')
+seq_prot = read_fasta('test_prot.fasta')
+
+def count_word(seq,word):
+    word_count=0
+    for i in range(len(seq)):
+        if seq[i] == word[0]:
+            if seq[i:(i+len(word))] == word:
+                      word_count+=1
+    return word_count
+
+def match_id(peptide,prot_file):
+    match_dico = {}
+    prot_dico=read_fasta(prot_file)
+    for prot in prot_dico:
+        n = count_word(prot_dico[prot],peptide)
+        #print(prot_dico[prot],"\n")
+        #print(peptide)
+        #print(n)
+        list_match = []
+        if n > 0 :
+            list_match.append(prot_dico[prot])
+        if n == 0 :
+            list_match.append('None')
+    return list_match
+
+def unique_match_set(peptide_file, prot_file):
+    peptide_dico= read_fasta(peptide_file)
+    #prot_dico = read_fasta(prot_file)
+    the_dico = {}
+    for peptide in peptide_dico : 
+        match = match_id(peptide_dico[peptide],prot_file) #retourne une liste de prots
+        for i in range(len(match)): 
+            if match[i] in dict.keys(the_dico):
+                the_list = the_dico[match[i]]
+                the_list.append(peptide_dico[peptide])
+                the_dico[match[i]]=the_list
+            else:
+                the_list = []
+                the_list.append(peptide_dico[peptide])
+                the_dico[match[i]]=the_list
+    return the_dico
+            
+a = unique_match_set('small_peptides.fa', 'human_blood_proteome.fa')
+
+
 
 
 
