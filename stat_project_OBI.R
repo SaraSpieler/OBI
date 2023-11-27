36b32f20198a034cca16c872198547ef60edbb04
Lucien-Piat <134285376+Lucien-Piat@users.noreply.github.com>
2023-11-27 17:51:14 +0100

    R snipet used for a prop test

--- stat_project_OBI.R
+++ stat_project_OBI.R
@@ -1,7 +1,7 @@
 #Stats OBI 
 
-nb_prot_totales <- 100
-nb_prot_identifies <- 90
+nb_prot_totales <- 772
+nb_prot_identifies <- 365
 c = matrix(c(c(nb_prot_totales,nb_prot_identifies),c(0,(nb_prot_totales-nb_prot_identifies))),nrow = 2, ncol = 2)
-p = prop.test(c,alternative = c("less"))
+p = prop.test(c,alternative = c("greater"))
 print(p)
